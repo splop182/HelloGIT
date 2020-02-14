1f0bd5dfba3a9d1be2069b93cd1e1bcbe9ffc083
Carlos A. Diaz Pingo <carlos.diazpingo@gmail.com>
2020-02-13 22:45:21 -0500

    clase de java

---
+++ JAVA PRUEBA/MiEjemplo/src/ClaseMath.java
@@ -0,0 +1,8 @@
+
+public class ClaseMath {
+     public static void main (String args[])
+     {
+     final int cons=10;
+     
+     }
+}
---
+++ JAVA PRUEBA/MiEjemplo/src/Variable.java
@@ -0,0 +1,12 @@
+public class Variable {
+
+    public static void main (String args[])
+    {
+    int Num;
+    Num = 9;
+    
+    System.out.print("El numero es "+Num);
+    
+    }
+    
+}
